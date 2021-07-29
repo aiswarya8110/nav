4f953bca9678707075ffac6d3c5a4ae0721c7106
aiswarya8110 <saish8110@gmail.com>
2021-07-29 22:50:53 +0530

    nav change

--- script.js
+++ script.js
@@ -1,3 +1,25 @@
-window.onload=function(){
-    document.body.requestFullscreen();
+var elem = document.documentElement;
+
+openFullscreen();
+
+/* View in fullscreen */
+function openFullscreen() {
+  if (elem.requestFullscreen) {
+    elem.requestFullscreen();
+  } else if (elem.webkitRequestFullscreen) { /* Safari */
+    elem.webkitRequestFullscreen();
+  } else if (elem.msRequestFullscreen) { /* IE11 */
+    elem.msRequestFullscreen();
+  }
+}
+
+/* Close fullscreen */
+function closeFullscreen() {
+  if (document.exitFullscreen) {
+    document.exitFullscreen();
+  } else if (document.webkitExitFullscreen) { /* Safari */
+    document.webkitExitFullscreen();
+  } else if (document.msExitFullscreen) { /* IE11 */
+    document.msExitFullscreen();
+  }
 }
